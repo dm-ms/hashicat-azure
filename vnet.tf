72f924d8d6fe98554fdb2f3503b0561b057a1269
Your Name <you@example.com>
2021-06-29 15:08:46 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -1,14 +1,14 @@
-#provider "azurerm" {
-#  features {}
-#}
-
-module "network" {
-  source  = "iacdev.wpp.cloud/WPP-Lab/network/azurerm"
-  version = "3.0.1"
-  # insert required variables here
-
-resource_group_name = azurerm_resource_group.example.name
+provider "azurerm" {
+  features {}
 }
+
+#module "network" {
+#  source  = "iacdev.wpp.cloud/WPP-Lab/network/azurerm"
+#  version = "3.0.1"
+#  # insert required variables here
+#
+#resource_group_name = azurerm_resource_group.example.name
+#}
 
 
 resource "azurerm_resource_group" "example" {
@@ -16,23 +16,23 @@ resource "azurerm_resource_group" "example" {
   location = "West Europe"
 }
 
-#module "network" {
-#  source              = "Azure/network/azurerm"
-#  resource_group_name = azurerm_resource_group.example.name
-#  address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
-#  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-#  subnet_names        = ["subnet1", "subnet2", "subnet3"]
-#
-#  subnet_service_endpoints = {
-#    "subnet1" : ["Microsoft.Sql"], 
-#    "subnet2" : ["Microsoft.Sql"],
-#    "subnet3" : ["Microsoft.Sql"]
-#  }
-#
-#  tags = {
-#    environment = "dev"
-#    costcenter  = "it"
-#  }
-#
-#  depends_on = [azurerm_resource_group.example]
-#}
+module "network" {
+  source              = "Azure/network/azurerm"
+  resource_group_name = azurerm_resource_group.example.name
+  address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_names        = ["subnet1", "subnet2", "subnet3"]
+
+  subnet_service_endpoints = {
+    "subnet1" : ["Microsoft.Sql"], 
+    "subnet2" : ["Microsoft.Sql"],
+    "subnet3" : ["Microsoft.Sql"]
+  }
+
+  tags = {
+    environment = "dev"
+    costcenter  = "it"
+  }
+
+  depends_on = [azurerm_resource_group.example]
+}
